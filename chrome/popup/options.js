25fa5ef25764a9653b3891f6aacd46ded218750c
Захар Смирнов <113017705+Basedfloppa@users.noreply.github.com>
2024-11-01 03:40:53 +0300

    version bump, separated main button color into separate group, tweaked ui

--- chrome/popup/options.js
+++ chrome/popup/options.js
@@ -1,47 +1,49 @@
-let red = document.getElementById("red");
-let blue = document.getElementById("blue");
-let green = document.getElementById("green");
+const elements = {
+  sliders: {
+    red: document.getElementById("red"),
+    blue: document.getElementById("blue"),
+    green: document.getElementById("green"),
+    hue: document.getElementById("hue"),
+    saturation: document.getElementById("saturation"),
+    lightness: document.getElementById("lightness")
+  },
+  values: {
+    redVal: document.getElementById("redValue"),
+    blueVal: document.getElementById("blueValue"),
+    greenVal: document.getElementById("greenValue"),
+    hueVal: document.getElementById("hueValue"),
+    saturationVal: document.getElementById("saturationValue"),
+    lightnessVal: document.getElementById("lightnessValue")
+  },
+  hex: document.getElementById("hex"),
+  swatch: document.getElementById("colorSwatch"),
+  colorId: document.getElementById("colorId"),
+  colorLabel: document.getElementById("colorLabel"),
+  buttons: document.querySelectorAll('.table button')
+};
 
-let redVal = document.getElementById("redValue");
-let blueVal = document.getElementById("blueValue");
-let greenVal = document.getElementById("greenValue");
+// Event Listeners
+Object.values(elements.sliders).forEach(slider => slider.addEventListener("input", updateColor));
+elements.hex.addEventListener("input", updateColor);
 
-let hue = document.getElementById("hue");
-let saturation = document.getElementById("saturation");
-let lightness = document.getElementById("lightness");
-
-let hueVal = document.getElementById("hueValue");
-let saturationVal = document.getElementById("saturationValue");
-let lightnessVal = document.getElementById("lightnessValue");
-
-let hex = document.getElementById("hex");
-
-let swatch = document.getElementById("colorSwatch");
-
-let colorName = document.getElementById("colorName");
-
-colorName.addEventListener("input", setColor);
-
-red.addEventListener("input", updateColor);
-green.addEventListener("input", updateColor);
-blue.addEventListener("input", updateColor);
-
-hue.addEventListener("input", updateColor);
-saturation.addEventListener("input", updateColor);
-lightness.addEventListener("input", updateColor);
-
-hex.addEventListener("input", updateColor);
+elements.buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const svgElement = button.querySelector('svg');
+    if (svgElement) {
+      elements.colorId.innerHTML = svgElement.id;
+      setColor(svgElement.id);
+    }
+  });
+});
 
 async function init() {
-  const options = Array.from(colorName.options);
-  const colors = await Promise.all(
-    options.map(option => getStorageValue(option.value) ?? "rgb(0,0,0)")
-  );
-  colors.forEach((color, i) => {
-    document.getElementById(options[i].value).style["background-color"] = color;
-  });
-  setColor();
+  const options = Array.from(document.querySelectorAll("div svg")).map(x => x.id);
+  for (let option of options) {
+    document.getElementById(option).style.fill = await getStorageValue(option) ?? "rgb(0,0,0)";
+  }
+  setColor(elements.colorId.innerHTML);
 }
+
 function getStorageValue(key) {
   return new Promise((resolve) => {
     chrome.storage.local.get([key], (result) => {
@@ -49,75 +51,75 @@ function getStorageValue(key) {
     });
   });
 }
+
 function updateColor() {
-  let tab = Array.from(document.getElementById("colorTab").childNodes).filter(x => x.type != undefined && Array.from(x.firstElementChild.classList).includes("active"))[0].firstElementChild.id.split("-")[0];
+  const activeTab = document.querySelector("#colorTab .nav-item .active")?.id.split("-")[0];
   let color = "";
 
-  switch (tab) {
+  switch (activeTab) {
     case "rgb":
-      color = "rgb(" + red.value + "," + green.value + "," + blue.value + ")";
+      color = `rgb(${elements.sliders.red.value}, ${elements.sliders.green.value}, ${elements.sliders.blue.value})`;
       updateRgbVal();
       break;
     case "hsl":
-      color = convertToRgb("hsl(" + hue.value + "," + saturation.value + "%," + lightness.value + "%)");
-      color = "rgb(" + color['r'] + "," + color['g'] + "," + color['b'] + ")";
+      const hslRgb = convertToRgb(`hsl(${elements.sliders.hue.value}, ${elements.sliders.saturation.value}%, ${elements.sliders.lightness.value}%)`);
+      color = `rgb(${hslRgb.r}, ${hslRgb.g}, ${hslRgb.b})`;
       updateHslVal();
       break;
     case "hex":
-      color = hexToRgb(hex.value);
-      color = "rgb(" + color['r'] + "," + color['g'] + "," + color['b'] + ")";
+      const hexRgb = hexToRgb(elements.hex.value);
+      color = `rgb(${hexRgb.r}, ${hexRgb.g}, ${hexRgb.b})`;
       break;
   }
 
-  swatch.style["background-color"] = color;
-  document.getElementById(colorName.value).style["background-color"] = color;
-
-  let variable = { Name: colorName.value, Value: color };
-  chrome.storage.local.set({ [variable.Name]: variable.Value });
-
+  document.getElementById(elements.colorId.innerHTML).style.fill = color;
+  elements.swatch.style.backgroundColor = color;
+  chrome.storage.local.set({ [elements.colorId.innerHTML]: color });
   chrome.runtime.sendMessage({ type: 'applyTheme' });
 }
-async function setColor() {
-  let color = await getStorageValue(colorName.value) ?? "rgb(0,0,0)";
-  color = convertToRgb(color) ?? { 'r': '0', 'g': '0', 'b': '0' };
 
-  swatch.style["background-color"] = "rgb(" + color['r'] + "," + color['g'] + "," + color['b'] + ")";
+async function setColor(id) {
+  id = id ?? elements.colorId.innerHTML;
+  let color = await getStorageValue(id) || "rgb(0,0,0)";
+  const colorRgb = convertToRgb(color) || { r: '0', g: '0', b: '0' };
 
-  red.value = color['r'];
-  green.value = color['g'];
-  blue.value = color['b'];
+  elements.colorLabel.innerHTML = `Picked: ${id.replace('ColorMap--', ' ').replaceAll('-', ' ')}`;
+  elements.swatch.style.backgroundColor = `rgb(${colorRgb.r}, ${colorRgb.g}, ${colorRgb.b})`;
+
+  elements.sliders.red.value = colorRgb.r;
+  elements.sliders.green.value = colorRgb.g;
+  elements.sliders.blue.value = colorRgb.b;
   updateRgbVal();
 
-  let hslColor = rgbToHsl(color['r'], color['g'], color['b']);
-  hue.value = hslColor['h'];
-  saturation.value = hslColor['s'];
-  lightness.value = hslColor['l'];
+  const hslColor = rgbToHsl(colorRgb.r, colorRgb.g, colorRgb.b);
+  elements.sliders.hue.value = hslColor.h;
+  elements.sliders.saturation.value = hslColor.s;
+  elements.sliders.lightness.value = hslColor.l;
   updateHslVal();
 
-  hex.value = rgbToHex(color['r'], color['g'], color['b']);
+  elements.hex.value = rgbToHex(colorRgb.r, colorRgb.g, colorRgb.b);
 }
+
 async function reset() {
-  const options = Array.from(colorName.options).map(x => x.value);
-
+  const options = Array.from(document.querySelectorAll("div svg")).map(x => x.id);
   for (let option of options) {
-    colorName.value = option;
-
-    await setColor(); 
+    elements.colorId.innerHTML = option;
+    await setColor(option);
     await updateColor();
-    await init(); 
+    await init();
   }
 }
 
-
 function updateRgbVal() {
-  redVal.innerHTML = red.value;
-  greenVal.innerHTML = green.value;
-  blueVal.innerHTML = blue.value;
+  elements.values.redVal.innerHTML = elements.sliders.red.value;
+  elements.values.greenVal.innerHTML = elements.sliders.green.value;
+  elements.values.blueVal.innerHTML = elements.sliders.blue.value;
 }
+
 function updateHslVal() {
-  hueVal.innerHTML = hue.value;
-  saturationVal.innerHTML = saturation.value;
-  lightnessVal.innerHTML = lightness.value;
+  elements.values.hueVal.innerHTML = elements.sliders.hue.value;
+  elements.values.saturationVal.innerHTML = elements.sliders.saturation.value;
+  elements.values.lightnessVal.innerHTML = elements.sliders.lightness.value;
 }
 
 document.addEventListener("DOMContentLoaded", init);
@@ -135,6 +137,7 @@ function hexToRgb(hex) {
     b: bigint & 255
   };
 }
+
 // Convert RGB to Hex
 function rgbToHex(r, g, b) {
   return (
@@ -147,6 +150,7 @@ function rgbToHex(r, g, b) {
       .join("")
   );
 }
+
 // Convert HSL to RGB
 function hslToRgb(h, s, l) {
   s /= 100;
@@ -160,6 +164,7 @@ function hslToRgb(h, s, l) {
     b: Math.round(f(4) * 255)
   };
 }
+
 // Convert RGB to HSL
 function rgbToHsl(r, g, b) {
   r /= 255;
@@ -189,17 +194,20 @@ function rgbToHsl(r, g, b) {
   }
   return { h: Math.round(h), s: Math.round(s * 100), l: Math.round(l * 100) };
 }
+
 // Convert Hex to HSL
 function hexToHsl(hex) {
   const { r, g, b } = hexToRgb(hex);
   return rgbToHsl(r, g, b);
 }
+
 // Parse an RGB string to RGB object
 function parseRgbString(rgbString) {
   const match = rgbString.match(/\d+/g);
   if (!match) return null;
   return { r: parseInt(match[0]), g: parseInt(match[1]), b: parseInt(match[2]) };
 }
+
 // Convert color string to RGB
 function convertToRgb(color) {
   if (color.startsWith("hsl")) {
@@ -217,7 +225,7 @@ function convertToRgb(color) {
 // #region PreMadeThemes
 const purpleLight = document.getElementById("PurpleLight");
 if (purpleLight) {
-    purpleLight.addEventListener("click", PurpleLight);
+  purpleLight.addEventListener("click", PurpleLight);
 }
 function PurpleLight() {
   chrome.storage.local.set({
@@ -229,9 +237,11 @@ function PurpleLight() {
     "ColorMap--content-warnings-hover": '#baa3e7',
     "ColorMap--text-primary": '#000000',
     "ColorMap--text-secondary": '#535353',
-    "ColorMap--border-color": '#000000'
+    "ColorMap--border-color": '#000000',
+    "ColorMap--main-button-text": '#000000'
   }, reset);
 }
+
 const purpleDim = document.getElementById("PurpleDim");
 if (purpleDim) {
   purpleDim.addEventListener("click", PurpleDim);
@@ -246,9 +256,11 @@ function PurpleDim() {
     "ColorMap--content-warnings-hover": '#4b435b',
     "ColorMap--text-primary": '#fff',
     "ColorMap--text-secondary": '#7f7f7f',
-    "ColorMap--border-color": 'rgb(46, 64, 82)'
+    "ColorMap--border-color": 'rgb(46, 64, 82)',
+    "ColorMap--main-button-text": '#fff'
   }, reset);
 }
+
 const purpleDark = document.getElementById("PurpleDark");
 if (purpleDark) {
   purpleDark.addEventListener("click", PurpleDark);
@@ -263,8 +275,9 @@ function PurpleDark() {
     "ColorMap--content-warnings-hover": '#3c2c57',
     "ColorMap--text-primary": '#fff',
     "ColorMap--text-secondary": '#8f9eb7',
-    "ColorMap--border-color": '#ffffff'
+    "ColorMap--border-color": '#ffffff',
+    "ColorMap--main-button-text": '#fff'
   }, reset);
 }
 // #endregion
-// #endregions
+// #endregion
--- firefox/background.js
+++ firefox/background.js
@@ -10,8 +10,10 @@ function init() {
     localStorage.setItem("ColorMap--text-primary", '#fff');
     localStorage.setItem("ColorMap--text-secondary", '#7f7f7f');
     localStorage.setItem("ColorMap--border-color", 'rgb(46, 64, 82)');
+    localStorage.setItem("ColorMap--main-button-text", "#fff");
   }
 }
+
 function getMap() {
   const colorMap = {
     '--accent-color': localStorage.getItem("ColorMap--accent-color"),
@@ -22,79 +24,17 @@ function getMap() {
     '--content-warnings-hover': localStorage.getItem("ColorMap--content-warnings-hover"),
     '--text-primary': localStorage.getItem("ColorMap--text-primary"),
     '--text-secondary': localStorage.getItem("ColorMap--text-secondary"),
-    '--border-color': localStorage.getItem("ColorMap--border-color")
+    '--border-color': localStorage.getItem("ColorMap--border-color"),
+    '--main-button-text': localStorage.getItem("ColorMap--main-button-text")
   };
 
   return colorMap;
 }
-const themes = {
-  'dimTheme': {
-    '--accent-color-val1': 'rgb(32, 139, 254)',
-    '--accent-color-val2': '#0085ff',
-    '--accent-color-val3': 'rgb(76, 162, 254)',
-    '--accent-color-val4': 'hsl(211, 20%, 95.3%)',
-    '--accent-color-val5': 'rgb(0, 133, 255)',
-    '--accent-color-hover-val1': 'rgb(76, 162, 254)',
-    '--accent-color-hover-val2': 'rgb(19, 63, 109)',
-    '--content-warnings-val': 'rgb(30, 41, 54)',
-    '--content-warnings-hover-val': 'rgb(38, 53, 68)',
-    '--background-change-val': 'rgb(22, 30, 39)',
-    '--text-primary-val1': 'rgb(241, 243, 245)',
-    '--text-primary-val2': 'hsl(211, 20%, 95.3%)',
-    '--text-secondary-val1': 'rgb(140, 158, 178)',
-    '--text-secondary-val2': 'hsl(211, 20%, 53%)',
-    '--text-secondary-val3': 'rgb(20, 27, 35)',
-    '--text-secondary-val4': 'rgb(212, 219, 226)',
-    '--text-secondary-val5': 'hsl(211, 20%, 62.4%)',
-    '--border-color-val': 'rgb(46, 64, 82)',
-  },
-  'darkTheme': {
-    '--accent-color-val1': 'rgb(16, 131, 254)',
-    '--accent-color-val2': '#0085ff',
-    '--accent-color-val3': 'rgb(52, 150, 254)',
-    '--accent-color-val4': 'hsl(211, 99%, 53%)',
-    '--accent-color-val4': 'hsl(211, 99%, 56%)',
-    '--accent-color-hover-val1': 'rgb(52, 150, 254)',
-    '--accent-color-hover-val2': 'rgb(19, 63, 109)',
-    '--content-warnings-val': 'rgb(20, 27, 35)',
-    '--content-warnings-hover-val': 'rgb(28, 39, 50)',
-    '--background-change-val': 'rgb(0, 0, 0)',
-    '--text-primary-val1': 'rgb(241, 243, 245)',
-    '--text-primary-val2': 'hsl(211, 20%, 95.3%)',
-    '--text-secondary-val1': 'rgb(140, 158, 178)',
-    '--text-secondary-val2': 'hsl(211, 20%, 53%)',
-    '--text-secondary-val3': 'rgb(20, 27, 35)',
-    '--text-secondary-val4': 'rgb(212, 219, 226)',
-    '--text-secondary-val5': 'hsl(211, 20%, 62.4%)',
-    '--border-color-val': 'rgb(37, 51, 66)',
-  },
-  'lightTheme': {
-    '--accent-color-val1': 'rgb(16, 131, 254)',
-    '--accent-color-val2': '#0085ff',
-    '--accent-color-val3': 'rgb(1, 104, 213)',
-    '--accent-color-val4': 'hsl(211, 99%, 53%)',
-    '--accent-color-val5': 'hsl(211, 28%, 6%)',
-    '--accent-color-hover-val1': 'rgb(1, 104, 213)',
-    '--accent-color-hover-val2': 'rgb(19, 63, 109)',
-    '--content-warnings-val': 'rgb(241, 243, 245)',
-    '--content-warnings-hover-val': 'rgb(226, 231, 236)',
-    '--background-change-val': 'rgb(255, 255, 255)',
-    '--text-primary-val1': 'rgb(11, 15, 20)',
-    '--text-primary-val2': 'hsl(211, 28%, 6%)',
-    '--text-secondary-val1': 'rgb(66, 87, 108)',
-    '--text-secondary-val2': 'hsl(211, 20%, 53%)',
-    '--text-secondary-val3': 'rgb(11, 15, 20)',
-    '--text-secondary-val4': 'rgb(37, 51, 66)',
-    '--text-secondary-val5': 'hsl(211, 24%, 34.2%)',
-    '--border-color-val': 'rgb(212, 219, 226)'
-  }
-};
 
 window.browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'applyTheme') {
     let map = getMap();
-    let theme = themes[message.themeName];
-    let response = { "ColorMap": map, 'Theme': theme };
+    let response = { "ColorMap": map };
 
     // Send message to the content script of the active tab
     window.browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
